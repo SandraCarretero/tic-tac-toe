5a751e77c964fd2eff33a399a75a0575fd3df049
SandraCarretero <sandracarretero24@gmail.com>
2024-02-14 10:33:07 +0100

    module

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,1 +1,18 @@
 // Aquí van los getElementById y los querySelectorAll que pondrías al principio.
+const gameElement = document.getElementById('game');
+const youScoreElement = document.getElementById('you');
+const cpuScoreElement = document.getElementById('cpu');
+const attemptsElement = document.getElementById('attempts');
+const returnButtonElement = document.getElementById('return-button');
+const popUpElement = document.getElementById('pop-up');
+const winnerElement = document.getElementById('winner');
+
+export {
+	gameElement,
+	youScoreElement,
+	cpuScoreElement,
+	attemptsElement,
+	returnButtonElement,
+	popUpElement,
+	winnerElement
+};
